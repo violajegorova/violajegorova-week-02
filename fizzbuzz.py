6ae8974027355619f991c2f8e27f0b65bd9fc780
violajegorova <violajegorova@inbox.lv>
2026-02-22 01:19:48 +0200

    feat: add fizzbuzz with sys.argv support

---
+++ fizzbuzz.py
@@ -0,0 +1,25 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Norādi skaitli N")
+    sys.exit(1)
+
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N jābūt skaitlim")
+    sys.exit(1)
+
+for i in range(1, N + 1):
+    if i % 3 == 0 and i % 5 == 0 and i % 7 == 0:
+        print("FizzBuzzJazz")
+    elif i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 7 == 0:
+        print("Jazz")
+    else:
+        print(i)
