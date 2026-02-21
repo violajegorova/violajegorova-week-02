326f681d373a9deab9926b8c19af93e8beb6204e
violajegorova <violajegorova@inbox.lv>
2026-02-21 20:02:16 +0200

    feat: initial commit

---
+++ converter.py
@@ -0,0 +1,18 @@
+# KONSTANTES
+KM_TO_MI = 0.621371             # 1 km = 0.621371 miles
+KG_TO_LB = 2.20462              # 1 kg = 2.20462 pounds
+L_TO_GAL = 0.264172             # 1 liter = 0.264172 gallons
+DOLLAR_TO_EURO = 0.84235020     # 1 dollar = 0.84235020 euros
+
+# Pajautā konversijas tipu/virzienu
+choice = input("Izvēlies konversiju (km_to_mi, mi_to_km): ").strip().lower()
+
+# Pajautā vērtību
+raw_value = input("Ievadi vērtību: ")
+
+# Nepareiza ievade
+try:
+    value = float(raw_value)
+    print(f"Ievadītā vērtība: {value:.2f}")
+except ValueError:
+    print("Kļūda: Ievadītā vērtība nav skaitlis!")
---
+++ type_explorer.py
@@ -0,0 +1,24 @@
+# Pamata tipu izpēte Pythonā
+name = "Violeta"
+year = 2026
+number = 99.99
+it_is_hot = False
+result = None
+
+#Izvade ar katras vērtības tipu
+print(name, type(name))
+print(year, type(year))
+print(number, type(number))
+print(it_is_hot, type(it_is_hot))
+print(result, type(result))
+
+#Pārbaude, vai vērtības ir patiesas (truthy) vai nepatiesas (falsy)
+print(bool("It is very hot"))       #Netukšs teksts -> True (truthy)
+print(bool(""))                     #Tukšs teksts -> False (falsy)
+print(bool(42))                     #Netukšs skaitlis -> True (truthy)
+
+#Konvertēšana starp tipiem
+print(float("5.976"))               #Konvertē tekstu "5.976" uz skaitli 5.976
+print(int("57"))                    #Konvertē tekstu "57" uz skaitli 57
+print(float("hello"))               #Nevar konvertēt tekstu "hello" uz skaitli, tāpēc tas izraisīs kļūdu (ValueError)
+print(str("42"))                    #Konvertē skaitli 42 uz tekstu "42"
